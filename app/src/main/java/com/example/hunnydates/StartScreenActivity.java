145c060db396753b5e21fea4af9a1d510861b4b8
Justin Mabutas <justin.troy.mabutas@gmail.com>
2020-11-17 10:41:51 -0800

    Created a bunch of xml files.

--- app/src/main/java/com/example/hunnydates/StartScreenActivity.java
+++ app/src/main/java/com/example/hunnydates/StartScreenActivity.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class HomeScreenActivity extends AppCompatActivity {
+public class StartScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.client_home_screen);
+//        setContentView(R.layout.start_screen);
+        setContentView(R.layout.admin_home_screen);
     }
 }
