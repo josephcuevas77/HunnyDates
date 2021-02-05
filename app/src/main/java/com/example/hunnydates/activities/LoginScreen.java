36ddbd7f9eea58d063db0657b5f4831ffdc26be6
Joseph Cuevas <josephcuevas77@outlook.com>
2021-02-04 19:04:08 -0800

    Google Login SDK and button Login Screen

---
+++ app/src/main/java/com/example/hunnydates/activities/LoginScreen.java
@@ -0,0 +1,16 @@
+package com.example.hunnydates.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hunnydates.R;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
