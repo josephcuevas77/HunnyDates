f257011f68004c25be7e7149dde8c1f4832a7fc7
Justin Mabutas <justin.troy.mabutas@gmail.com>
2021-02-04 18:10:58 -0800

    Added filler files for project directory structure.

--- app/src/main/java/com/example/hunnydates/activities/StartScreenActivity.java
+++ app/src/main/java/com/example/hunnydates/activities/StartScreenActivity.java
@@ -1,8 +1,10 @@
-package com.example.hunnydates;
+package com.example.hunnydates.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.hunnydates.R;
 
 public class StartScreenActivity extends AppCompatActivity {
 
